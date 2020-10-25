886f85618050b20cc6fa4196053f85b7d1119316
Tor Nordam <tor.nordam@sintef.no>
2020-10-25 10:27:07 +0100

    Changed aspect ratio of plot

--- src/plot_both.py
+++ src/plot_both.py
@@ -7,6 +7,8 @@ from matplotlib import pyplot as plt
 times_py, variance_py   = np.loadtxt('../output/variance_python.txt').T
 times_f90, variance_f90 = np.loadtxt('../output/variance_fortran.txt').T
 
+fig = plt.figure(figsize = (7,4))
+
 plt.plot(times_py,  variance_py,  label = 'Python result')
 plt.plot(times_f90, variance_f90, '--', label = 'Fortran result')
 
